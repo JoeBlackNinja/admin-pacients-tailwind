963ea31b115b73c842d2cab26c65e8beade065e3
JoeBlack <jose.nava@alumnos.udg.mx>
2022-03-25 19:54:27 -0600

    Version 1 only labels and learning how to use Tailwindcss

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,13 @@
+const Formulario = () =>{
+    return(
+        <div className="md:w-1/2 lg:w-3/3">
+            <h3 className="font-black text-2xl text-center">Seguimiento pacientes</h3>
+            <p className="text-lg mt-5 text-center">
+                Añade pacientes y 
+                <span className="text-indigo-600 font-bold"> Administralos</span>
+            </p>
+        </div>
+    );
+}
+
+export default Formulario;
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,9 @@
+const ListadoPacientes = () => {
+  return (
+    <div className="md:w-1/2 lg:w-2/3">
+      <h1>Listado Pacientes</h1>
+    </div>
+  )
+}
+
+export default ListadoPacientes;
