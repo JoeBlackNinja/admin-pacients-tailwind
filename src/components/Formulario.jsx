9b4aa0870f43c386eade4337126a51bda9fb23af
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-03-28 19:49:49 -0600

    Agregado del boton de llenado de formulario con info erronea en color rojo

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,8 +2,25 @@ import { useState, useEffect } from "react";
 
 const Formulario = () =>{
     const [nombre,setNombre] = useState('');
+    const [propietario,setPropietario] = useState('');
+    const [correo,setCorreo] = useState('');
+    const [fecha,setFecha] = useState('');
+    const [sintomas,setSintomas] = useState('');    
 
-    console.log('Cantilever');
+    const [error,setError] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        //Validacion de los datos de entrada
+        if([nombre,propietario,correo,fecha,sintomas].includes('')){
+            console.log("No se permiten campos vacios");
+
+            setError(true);
+        } else {
+            setError(false);
+        }
+    }
 
     return(
         <div className="md:w-1/2 lg:w-3/3">
@@ -12,7 +29,17 @@ const Formulario = () =>{
                 Añade pacientes y 
                 <span className="text-indigo-600 font-bold"> Administralos</span>
             </p>
-            <form  className="bg-white shadow-md rounded-lg py-10 px-4">
+            <form  className="bg-white shadow-md rounded-lg py-10 px-4"
+                    onSubmit={handleSubmit}>
+
+                {error && 
+                    <div
+                        className="bg-red-600 text-white text-center p-3 
+                        uppercase font-bold mb-3 rounded-md"
+                    ><p>Todos los campos son obligatorios</p>
+                    </div>
+                }
+
                 <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-600 uppercase font-bold">Nombre mascota</label>
                     <input
@@ -31,6 +58,8 @@ const Formulario = () =>{
                         type="text"
                         placeholder="Ingresa tu nombre"
                         className="border-2 w-full p-2 mt-2 placeholder-indigo-700 rounded-md"
+                        value={propietario}
+                        onChange={ (e) => setPropietario(e.target.value) }
                     />
                 </div>
                 <div className="mb-5">
@@ -40,6 +69,8 @@ const Formulario = () =>{
                         type="email"
                         placeholder="Ingresa tu correo"
                         className="border-2 w-full p-2 mt-2 placeholder-indigo-700 rounded-md"
+                        value={correo}
+                        onChange={ (e) => setCorreo(e.target.value) }
                     />
                 </div>
                 <div className="mb-5">
@@ -48,6 +79,8 @@ const Formulario = () =>{
                         id="fecha" 
                         type="date"
                         className="border-2 w-full p-2 mt-2 placeholder-indigo-700 rounded-md"
+                        value={fecha}
+                        onChange={ (e) => setFecha(e.target.value) }
                     />
                 </div>
                 <div className="mb-5">
@@ -56,6 +89,8 @@ const Formulario = () =>{
                         id="sintomas"
                         className="border-2 w-full p-2 mt-2 placeholder-indigo-700 rounded-md"
                         placeholder="Desciba los sintomas"
+                        value={sintomas}
+                        onChange={ (e) => setSintomas(e.target.value) }
                     />                    
                 </div>
                 <input  
