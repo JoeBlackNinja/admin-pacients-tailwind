45af4db7423ec80270e16f7580a51d126ab1d5ff
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-04-02 21:01:31 -0600

    04-02-2022 Video 74 aprox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import ListadoPacientes from './components/ListadoPacientes';
+import {useState} from 'react';
 
 function App() {
+  const [pacientes,setPacientes] = useState([]);
+
   return (
     <div className="container mx-auto mt-10">
-       <Header/>
+       <Header
+        numeros={1}
+        isAdmin={false}
+       />
        <div className='mt-12 flex'>
          <Formulario/>
          <ListadoPacientes/>
