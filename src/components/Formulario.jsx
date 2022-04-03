6b2a719bc8416ba326c8a005710b2aa8ecf24052
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-04-02 23:55:57 -0600

    Video 75 min 00:00

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Error from "./Error";
 
 const Formulario = () =>{
     const [nombre,setNombre] = useState('');
@@ -32,13 +33,9 @@ const Formulario = () =>{
             <form  className="bg-white shadow-md rounded-lg py-10 px-4"
                     onSubmit={handleSubmit}>
 
-                {error && 
-                    <div
-                        className="bg-red-600 text-white text-center p-3 
-                        uppercase font-bold mb-3 rounded-md"
-                    ><p>Todos los campos son obligatorios</p>
-                    </div>
-                }
+                {error && <Error>No se permiten campos vacios</Error> }
+                    
+                
 
                 <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-600 uppercase font-bold">Nombre mascota</label>
