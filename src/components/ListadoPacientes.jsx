c19ce3731669c6b5cfeb4e2e427fd22f431a0042
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-04-08 20:49:31 -0500

    Video 80 00:00. Botones Editar y Eliminar con estilos. Etuiquetas para mostrar componentes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,16 +1,21 @@
 import Paciente from "./Paciente";
 
 const ListadoPacientes = ({pacientes}) => {
+
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2
-        className="font-black text-3xl text-center"
-      >Listado Pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra tus {''}
-        <span className="text-indigo-600 font-bold"
+
+    {pacientes && pacientes.length ? 
+      (
+        <>
+        <h2
+          className="font-black text-3xl text-center"
+        >Listado Pacientes</h2>
+        <p className="text-xl mt-5 mb-10 text-center">
+          Administra tus {''}
+          <span className="text-indigo-600 font-bold"
         >pacientes y citas</span>
-      </p>
+        </p>
 
       {pacientes.map( (paciente) => {
         return(
@@ -20,7 +25,20 @@ const ListadoPacientes = ({pacientes}) => {
           />
         )                  
       })}
+      </>
 
+      ) : (
+        <>
+          <h2
+            className="font-black text-3xl text-center"
+          >No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+           Comienza agregando pacientes {''}
+            <span className="text-indigo-600 font-bold"
+          >y apareceran en este lugar</span>
+          </p>
+        </>
+      )}
     </div>
   )
 }
